502129e97618969b97a2f3674b498743f2c2851b
ShimantoDevJS <shimantodevjs@gmail.com>
2023-09-14 10:07:29 +0300

    fixed text in rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -11,7 +11,7 @@ export default function Rightbar() {
         <Recommendation type="popular" />
         <Recommendation type="editor" />
         <button className="rightButton">
-          See What?
+          no way?
           <ArrowDropDown />
         </button>
       </div>
